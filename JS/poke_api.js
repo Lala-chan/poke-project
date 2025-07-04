745ac21dd71f69337009aeb96afa0a62a7916716
Chiara Di Modica <198486156+Lala-chan@users.noreply.github.com>
2025-07-04 12:07:55 +0200

    added a css to the poke-battle

--- JS/poke_api.js
+++ JS/poke_api.js
@@ -108,5 +108,19 @@ async function pokelist(params) {
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-pokelist()
+    pokelist()
+
+    const scrollBtn = document.getElementById("scrollTop")
+
+    window.onscroll = function() {scroll()}
+
+    scrollBtn.addEventListener("click", function (){
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+    })
+
+    function scroll(){
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+            scrollBtn.style.display = "block"
+        } else {scrollBtn.style.display = "none"}
+    }
 })
